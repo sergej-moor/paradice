23aafaf468bd3e81f69ff40d9ac0a0be4a42ce0c
Sergej Moor <sergejmoor01@gmail.com>
2024-11-26 00:53:53 +0100

    added native wind 🙏

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+// Import your global CSS file
+import "../global.css";
+
+export default Slot;
